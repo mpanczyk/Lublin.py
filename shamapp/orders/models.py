774272cf9d90500bd025b7871e95466cf9ef7e25
Michał Pańczyk <mjpanczyk@gmail.com>
2018-06-28 12:29:11 +0200

    Add first model

--- shamapp/orders/models.py
+++ shamapp/orders/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Provider(models.Model):
+    full_name = models.CharField(max_length=100)
+    url = models.URLField()
+    opening_hour = models.TimeField()
+    closing_hour = models.TimeField()
+    delivery_cost = models.DecimalField(
+        decimal_places=2,
+        max_digits=5,
+    )
+    minimal_order_amount = models.DecimalField(
+        decimal_places=2,
+        max_digits=5,
+    )
